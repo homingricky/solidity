01d148e86e8dca56fdc7ca42f0cfc0598a56598e
Ricky Cheng <rickycheng@113-28-44-24.static.imsbiz.com>
2022-04-15 17:26:56 +0800

    contract 6

--- contracts/6_SimpleAuction.sol
+++ contracts/6_SimpleAuction.sol
@@ -23,6 +23,51 @@ contract SimpleAuction {
         auctionEndTime = block.timestamp + _biddingTime;
     }
 
-    
+    // Payable is a modifier that ensures that the function can send and receive Ether
+    function bid() public payable{
+        if (block.timestamp > auctionEndTime) {
+            revert("The auction has already ended");
+        }
+
+        if (msg.value <= highestBid) {
+            revert("There is already a higher or equal bid");
+        }
+
+        if (highestBid != 0){
+            pendingReturns[highestBidder] += highestBid;
+        }
+
+        highestBidder = msg.sender;
+        highestBid = msg.value;
+        emit HighestBidIncrease(msg.sender, msg.value);
+    }
+
+    function withdraw() public returns(bool){
+        uint amount = pendingReturns[msg.sender];
+        if (amount > 0){
+            pendingReturns[msg.sender] = 0;
+            
+            if(!payable(msg.sender).send(amount)){
+                pendingReturns[msg.sender] = amount;
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function auctionEnd() private {
+        if (block.timestamp < auctionEndTime){
+            revert("The auction has not ended yet");
+        }
+        
+        if (ended){
+            revert("The function auctionEnded has already been called");
+        }
+
+        ended = true;
+        emit AuctionEnded(highestBidder, highestBid);
+
+        beneficiary.transfer(highestBid);
+    }
 
 }
